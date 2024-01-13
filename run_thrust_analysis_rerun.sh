41b4220c120b2515da3b199d9dcc6ccfd4d3b714
Brandon Apodaca <bapodaca@umich.edu>
2024-01-13 11:40:24 -0500

    all 1_1_1 solutions in. run scriptsput into run_scripts folder

--- run_thrust_analysis_rerun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python casadi_opti.py 0.2 'thrust_test_k_1_p_1_f_1' 1 1 1
-python casadi_opti.py 0.3 'thrust_test_k_1_p_1_f_1' 1 1 1
-python casadi_opti.py 0.4 'thrust_test_k_1_p_1_f_1' 1 1 1
-python casadi_opti.py 0.5 'thrust_test_k_1_p_1_f_1' 1 1 1
-python casadi_opti.py 0.6 'thrust_test_k_1_p_1_f_1' 1 1 1
-python casadi_opti.py 0.7 'thrust_test_k_1_p_1_f_1' 1 1 1
